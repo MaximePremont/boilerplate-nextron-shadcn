78723178fc8520593ddd8d71d6a96176d72889a4
MaximePremont <maxime.premont@gmail.com>
2024-09-09 10:25:21 +0200

    feat(boilerplate) : Added project

---
+++ renderer/pages/index.tsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+
+function IndexPage() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Nextron boilerplate</title>
+      </Head>
+      <div className='flex flex-col items-center gap-2 pt-10'>
+        <Image
+          priority={false}
+          src="/images/logo.png"
+          alt="logo"
+          width={150}
+          height={150} />
+        <h1 className='text-lg font-semibold'>Nextron ( Next.Js + Electron ) Boilerplate</h1>
+        <p>With TypeScript, TailwindCSS and Shadcn/ui</p>
+        <p>Crossbuild for Web or Desktop</p>
+      </div>
+    </Fragment>
+  )
+}
+
+export default IndexPage
